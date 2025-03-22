7bb5bb7edad7d8d8f2e48fb538c0482aef5dd097
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-22 20:33:08 +0600

    Sponsor part complete

--- src/components/Sponsorship.jsx
+++ src/components/Sponsorship.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import SponsorCard from '/SponsorCard.png'
+import AmazonCard from '/AmazonCard.png'
 
 const Sponsorship = () => {
   return (
-    <div>Sponsorship</div>
+    <>
+    <section className='bg-[#1E1E1E]'>
+      <div className="max-w-[1280px] mx-auto flex items-center justify-between py-8">
+        <img src={SponsorCard} alt="" />
+        <img src={AmazonCard} alt="" />
+        <img src={SponsorCard} alt="" />
+        <img src={AmazonCard} alt="" />
+        <img src={SponsorCard} alt="" />
+        <img src={AmazonCard} alt="" />
+      </div>
+    </section>
+    </>
   )
 }
 
