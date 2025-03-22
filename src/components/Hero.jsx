488390a01c3c2a49b2643789f00ae76ab1e30a0f
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-22 20:32:27 +0600

    Hero part complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import HeroBenner from '/HeroBenner.png'
+import Button from './Button'
 
 const Hero = () => {
   return (
     <>
       <section className='bg-[#1E1E1E]'>
         <div className="max-w-[1280px] mx-auto">
-          <div className="w-full">
-            <img src={HeroBenner} alt="" />
-            <div className="">
-              <h1 className='text-5xl font-semibold text-white mb-8'>A Digital Product Studio
+          <div className=" relative">
+            <img src={HeroBenner} alt="" className='w-full h-[691px]'/>
+            <div className="absolute top-[112px] right-[290px] flex flex-col items-center">
+              <h1 className='text-5xl font-semibold text-white mb-8 w-[510px] text-center'>A Digital Product Studio
                 that will Work</h1>
-              <p className='text-lg text-[#98989A] '>For  <span className='bg-[#262626] px-2.5 py-2 rounded-lg text-white'>Startups</span>  ,  <span>Enterprise leaders</span>  ,  <span>Media & Publishers</span>and<span>Social Good</span></p>
+              <div className="border px-[30px] py-[18px] bg-[#24242480] border-[#262626] w-[724px] mb-10">
+                <p className='text-lg text-[#98989A] '>For<span className='bg-[#262626] px-2.5 py-2 rounded-lg text-white mx-2'>Startups</span>,  <span className='bg-[#262626] px-2.5 py-2 rounded-lg text-white mx-2'>Enterprise leaders</span>,<span className='bg-[#262626] px-2.5 py-2 rounded-lg text-white mx-2'>Media & Publishers</span>and<span className='bg-[#262626] px-2.5 py-2 rounded-lg text-white mx-2'>Social Good</span></p>
+              </div>
+              <div className="flex items-center gap-x-3">
+                <Button/>
+                <Button/>
+              </div>
 
+            </div>
+            <div className="absolute top-[670px] right-[531px] pb-1.5">
+              <h5 className='w-[220px] text-sm font-medium text-[#FDFFFA] bg-[#1A1A1A] border px-6 py-3.5 border-[#262626] rounded-4xl'>Trusted By 250+ Companies</h5>
             </div>
           </div>
         </div>
