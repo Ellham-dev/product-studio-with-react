acdefa98a3bb4e5250e7dc8b53adb2672a8625b6
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-23 21:53:48 +0600

    SquareUp part benner complete

--- src/components/SquareUp.jsx
+++ src/components/SquareUp.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import SquareUpBenner from '/SquareUpBenner.png'
 
 const SquareUp = () => {
   return (
-    <div>SquareUp</div>
+    <>
+      <section className='bg-[#1E1E1E]'>
+        <div className="relative">
+          <div className="max-w-[1280px] mx-auto">
+            <img src={SquareUpBenner} alt="" />
+          </div>
+          <div className="absolute top-0 left-0 w-full h-full bg-black/80">
+            <div className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <h2 className='text-[38px] font-semibold text-white'>Why Choose SquareUp?</h2>
+              <p className='text-base leading-6 text-[#E6E6E6]'>Experience excellence in digital craftsmanship with our team of skilled professionals dedicated to delivering exceptional results.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
