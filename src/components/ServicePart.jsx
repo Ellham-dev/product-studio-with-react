29af9279eaa672b6f2c608467eca07b3ee982313
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-23 21:46:52 +0600

    Servive part complete

--- src/components/ServicePart.jsx
+++ src/components/ServicePart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ServiceBenner from '/ServiceBenner.png'
+import Serviceicon from '/ServiceIcon.png'
 
 const ServicePart = () => {
   return (
@@ -15,6 +16,26 @@ const ServicePart = () => {
               <p className='text-base leading-6 text-[#E6E6E6]'>Transform your brand with our innovative digital solutions that captivate and engage your audience.</p>
             </div>
           </div>
+        </div>
+        <div className="flex items-center">
+        <div className="border border-[#262626] p-10 w-[426.67px]">
+          <img src={Serviceicon} alt="" className='mb-[30px]'/>
+          <h3 className='text-2xl leading-[150%] font-semibold text-white mb-3.5'>Design</h3>
+          <p className='text-base leading-[150%] text-[#E6E6E6] mb-[84px]'>At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences.</p>
+          <button className='border w-full py-[14px]  border-[#262626] text-white'>Learn More</button>
+        </div>
+        <div className="border border-[#262626] p-10 w-[426.67px]">
+          <img src={Serviceicon} alt="" className='mb-[30px]'/>
+          <h3 className='text-2xl leading-[150%] font-semibold text-white mb-3.5'>Design</h3>
+          <p className='text-base leading-[150%] text-[#E6E6E6] mb-[84px]'>At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences.</p>
+          <button className='border w-full py-[14px]  border-[#262626] text-white'>Learn More</button>
+        </div>
+        <div className="border border-[#262626] p-10 w-[426.67px]">
+          <img src={Serviceicon} alt="" className='mb-[30px]'/>
+          <h3 className='text-2xl leading-[150%] font-semibold text-white mb-3.5'>Design</h3>
+          <p className='text-base leading-[150%] text-[#E6E6E6] mb-[84px]'>At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences.</p>
+          <button className='border w-full py-[14px]  border-[#262626] text-white'>Learn More</button>
+        </div>
         </div>
       </div>
     </section>
