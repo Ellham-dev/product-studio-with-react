1ddfcab3f7144d89b25390470fb8c15efeb38fb9
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-24 23:51:31 +0600

    input button create

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <>
     <section>
       <div className="max-w-[1280px]">
-        
+        <input type="text" placeholder='Your Number' className='w-full py-4 text-lg leading-4' />
       </div>
     </section>
     </>
