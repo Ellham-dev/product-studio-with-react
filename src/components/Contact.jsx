69117fcca3612d688d8e21ed6d7178c6d43501eb
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-30 13:54:45 +0600

    Selection part end

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,12 +11,30 @@ const Contact = () => {
               <input type="text" placeholder='Type here' className='text-base leading-[150%] border-b outline-none pb-1.5 text-[#656567] border-[#333333] w-[288px]' />
             </div>
             <div className="border px-[30px] py-[20px] border-[#262626] w-[348px] bg-[#242424] rounded-lg">
-              <h6 className='text-lg leading-[150%] font-medium text-white mb-4'>Full Name</h6>
-              <input type="text" placeholder='Type here' className='text-base leading-[150%] border-b outline-none pb-1.5 text-[#656567] border-[#333333] w-[288px]' />
+              <h6 className='text-lg leading-[150%] font-medium text-white mb-4'>Email</h6>
+              <input type="email" placeholder='Type here' className='text-base leading-[150%] border-b outline-none pb-1.5 text-[#656567] border-[#333333] w-[288px]' />
             </div>
           </div>
-          <div className="">
-            <h6></h6>
+          <div className="bg-[#242424] rounded-lg p-[30px] mb-[30px]">
+            <h6 className='text-lg leading-[150%] font-medium mb-[30px] text-white'>Why are you contacting us?</h6>
+            <div className="flex flex-wrap gap-7">
+            <div className="flex items-center gap-x-2.5 w-[323px]">
+            <input type="checkbox" className=''/>
+            <p className='text-base leading-[150%] text-white'>Web Design</p>
+            </div>
+            <div className="flex items-center gap-x-2.5 w-[323px]">
+            <input type="checkbox" className=''/>
+            <p className='text-base leading-[150%] text-white'>Web Design</p>
+            </div>
+            <div className="flex items-center gap-x-2.5 w-[323px]">
+            <input type="checkbox" className=''/>
+            <p className='text-base leading-[150%] text-white'>Web Design</p>
+            </div>
+            <div className="flex items-center gap-x-2.5 w-[323px]">
+            <input type="checkbox" className=''/>
+            <p className='text-base leading-[150%] text-white'>Web Design</p>
+            </div>
+            </div>
           </div>
         </div>
       </section>
