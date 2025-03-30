ef2e7f598b9129da264ddc61856ba9aa92a78220
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-30 13:34:15 +0600

    Contact page name email part end

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,11 +3,23 @@ import React from 'react'
 const Contact = () => {
   return (
     <>
-    <section>
-      <div className="max-w-[1280px]">
-        <input type="text" placeholder='Your Number' className='w-full py-4 text-lg leading-4' />
-      </div>
-    </section>
+      <section className='bg-[#1E1E1E]'>
+        <div className="max-w-[846px] mx-auto">
+          <div className="flex items-center justify-between mb-[30px]">
+            <div className="border px-[30px] py-[20px] border-[#262626] w-[348px] bg-[#242424] rounded-lg">
+              <h6 className='text-lg leading-[150%] font-medium text-white mb-4'>Full Name</h6>
+              <input type="text" placeholder='Type here' className='text-base leading-[150%] border-b outline-none pb-1.5 text-[#656567] border-[#333333] w-[288px]' />
+            </div>
+            <div className="border px-[30px] py-[20px] border-[#262626] w-[348px] bg-[#242424] rounded-lg">
+              <h6 className='text-lg leading-[150%] font-medium text-white mb-4'>Full Name</h6>
+              <input type="text" placeholder='Type here' className='text-base leading-[150%] border-b outline-none pb-1.5 text-[#656567] border-[#333333] w-[288px]' />
+            </div>
+          </div>
+          <div className="">
+            <h6></h6>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
