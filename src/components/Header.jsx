d3a5149af892da3a2aa153213074d080e3fa15e0
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-31 09:22:22 +0600

    color change navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Logo from '/Logo.png'
 import SquareUp from '/SquareUp.png'
 import Button from './Button'
-import { NavLink } from 'react-router'
+import { Link, NavLink } from 'react-router'
 
 const Header = () => {
     return (
@@ -11,28 +11,30 @@ const Header = () => {
                 <div className="">
                     <div className="max-w-[1280px] mx-auto flex items-center justify-between border-b border-[#262626]">
                         <div className="flex items-center gap-x-1.5 py-[22px] ">
-                            <img src={Logo} alt="" />
-                            <img src={SquareUp} alt="" />
+                            <Link to={"/"}><img src={Logo} alt="" /></Link>
+                            <Link to={"/"}><img src={SquareUp} alt="" /></Link>
+                            
+                            
                         </div>
                         <div className="text-[#E6E6E6]">
                             <ul className='flex items-center gap-x-6 text-sm leading-[150%] font-medium'>
                             <li><NavLink style={({ isActive }) => ({
-                                    color: isActive ? "blue" : "white", fontWeight: isActive ? "" : ""
+                                    color: isActive ? "green" : "white", fontWeight: isActive ? "" : ""
                                 })} to="/">Home</NavLink></li>
                                 <li><NavLink style={({ isActive }) => ({
-                                    color: isActive ? "blue" : "white", fontWeight: isActive ? "medium" :''
+                                    color: isActive ? "green" : "white", fontWeight: isActive ? "medium" :''
                                 })} to="service">Services</NavLink></li>
                                 <li><NavLink style={({ isActive }) => ({
-                                    color: isActive ? "blue" : "white", fontWeight: isActive ? "medium" :''
+                                    color: isActive ? "green" : "white", fontWeight: isActive ? "medium" :''
                                 })} to="work">Work</NavLink></li>
                                 <li><NavLink style={({ isActive }) => ({
-                                    color: isActive ? "blue" : "white", fontWeight: isActive ? "medium" :''
+                                    color: isActive ? "green" : "white", fontWeight: isActive ? "medium" :''
                                 })} to="process">Process</NavLink></li>
                                 <li><NavLink style={({ isActive }) => ({
-                                    color: isActive ? "blue" : "white", fontWeight: isActive ? "medium" :''
+                                    color: isActive ? "green" : "white", fontWeight: isActive ? "medium" :''
                                 })} to="about">About</NavLink></li>
                                 <li><NavLink style={({ isActive }) => ({
-                                    color: isActive ? "blue" : "white", fontWeight: isActive ? "medium" :''
+                                    color: isActive ? "green" : "white", fontWeight: isActive ? "medium" :''
                                 })} to="careers">Careers</NavLink></li>
                             </ul>
                         </div>
