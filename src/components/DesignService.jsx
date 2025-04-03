87a6ef4e4d20cc789541259e7d1c0409dd0c0c58
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-03 23:09:52 +0600

    DesignService part complete

--- src/components/DesignService.jsx
+++ src/components/DesignService.jsx
@@ -1,14 +1,78 @@
 import React from 'react'
+import DesignSIcon from '/DesignSIcon.png'
 
 const DesignService = () => {
   return (
     <>
       <section className='bg-[#1E1E1E]'>
         <div className="max-w-[1280px] mx-auto">
-          <div className="border-b border-[#333333] pb-10">
+          <div className="border-x border-[#333333] px-10 pt-20 pb-10">
             <h2 className='text-[38px] font-semibold mb-2.5 text-white'>Design</h2>
             <p className='text-base leading-[150%] text-[#E6E6E6] mb-10 w-[950px]'>At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences.</p>
             <span className='text-lg bg-[#262626] p-2.5 w-[239px] rounded-lg text-white'>Our design services include:</span>
+          </div>
+          <div className="">
+            <h3 className='text-[22px] font-medium text-[#98989A] border border-[#333333] px-10 py-10'>User Experience (UX) Design</h3>
+            <div className="flex items-center justify-between">
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <h3 className='text-[22px] font-medium text-[#98989A] border border-[#333333] px-10 py-10'>User Interface (UI) Design</h3>
+            <div className="flex items-center justify-between">
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <h3 className='text-[22px] font-medium text-[#98989A] border border-[#333333] px-10 py-10'>Branding and Identity</h3>
+            <div className="flex items-center justify-between">
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+              <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                <img src={DesignSIcon} alt="" className='mb-6'/>
+                <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
