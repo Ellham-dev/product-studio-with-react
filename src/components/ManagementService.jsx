91db718babe7e2b67f5e5b721191380802cdf17a
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-03 23:22:56 +0600

    ManagementService componenets complete

--- src/components/ManagementService.jsx
+++ src/components/ManagementService.jsx
@@ -1,8 +1,82 @@
 import React from 'react'
+import DesignSIcon from '/DesignSIcon.png'
 
 const ManagementService = () => {
   return (
-    <div>ManagementService</div>
+    <>
+    <section className='bg-[#1E1E1E]'>
+                <div className="max-w-[1280px] mx-auto">
+                  <div className="border-x border-[#333333] px-10 pt-20 pb-10">
+                    <h2 className='text-[38px] font-semibold mb-2.5 text-white'>Project Management</h2>
+                    <p className='text-base leading-[150%] text-[#E6E6E6] mb-10 w-[950px]'>Our experienced project management team ensures that your projects are delivered on time, within budget, and according to your specifications. We follow industry-standard methodologies and employ effective communication and collaboration tools to keep you informed throughout the development process.</p>
+                    <span className='text-lg bg-[#262626] p-2.5 w-[330px] rounded-lg text-white'>Our project management services include:</span>
+                  </div>
+                  <div className="">
+                    <h3 className='text-[22px] font-medium text-[#98989A] border border-[#333333] px-10 py-10'>Project Planning and Scoping</h3>
+                    <div className="flex items-center justify-between">
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="">
+                    <h3 className='text-[22px] font-medium text-[#98989A] border border-[#333333] px-10 py-10'>Agile Development</h3>
+                    <div className="flex items-center justify-between">
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="">
+                    <h3 className='text-[22px] font-medium text-[#98989A] border border-[#333333] px-10 py-10'>Quality Assurance and Testing</h3>
+                    <div className="flex items-center justify-between">
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                      <div className="border border-[#333333] px-10 py-10 w-[320px]">
+                        <img src={DesignSIcon} alt="" className='mb-6'/>
+                        <p className='text-base leading-[150%] font-medium text-[#E6E6E6] w-[240px] '>User Research and Persona Development</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+    </>
   )
 }
 
