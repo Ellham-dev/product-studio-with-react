c0be95fbb4cb8b70fbe5a03f31eb1817d0faaa01
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-03 22:41:39 +0600

    Design Service first part complete

--- src/components/DesignService.jsx
+++ src/components/DesignService.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const DesignService = () => {
   return (
-    <div>DesignService</div>
+    <>
+      <section className='bg-[#1E1E1E]'>
+        <div className="max-w-[1280px] mx-auto">
+          <div className="border-b border-[#333333] pb-10">
+            <h2 className='text-[38px] font-semibold mb-2.5 text-white'>Design</h2>
+            <p className='text-base leading-[150%] text-[#E6E6E6] mb-10 w-[950px]'>At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences.</p>
+            <span className='text-lg bg-[#262626] p-2.5 w-[239px] rounded-lg text-white'>Our design services include:</span>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
