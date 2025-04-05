8e70124c5796211911169827a34d2f4e6240c420
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-05 20:57:50 +0600

    Service hero complete

--- src/components/ServiceHero.jsx
+++ src/components/ServiceHero.jsx
@@ -13,7 +13,7 @@ const SVhero = () => {
                   <div className="absolute top-0 left-0 w-full h-full bg-black/80">
                     <div className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                       <h2 className='text-[38px] font-semibold text-white mb-2.5'>Our Services</h2>
-                      <p className='text-base leading-6 text-[#E6E6E6] w-[780px]'>Transform your brand with our innovative digital solutions that captivate and engage your audience.</p>
+                      <p className='text-lg leading-[150%] text-[#E6E6E6] w-[780px]'>Transform your brand with our innovative digital solutions that captivate and engage your audience.</p>
                     </div>
                   </div>
                 </div>
--- src/components/WorksHero.jsx
+++ src/components/WorksHero.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import ServiceBenner from '/ServiceBenner.png'
 
 const WorksHero = () => {
   return (
-    <div>WorksHero</div>
+    <>
+      <section className='bg-[#1E1E1E]'>
+        <div className="max-w-[1280px] mx-auto">
+          <div className="relative">
+            <div className="">
+              <img src={ServiceBenner} alt="" />
+            </div>
+            <div className="absolute top-0 left-0 w-full h-full bg-black/80">
+              <div className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <h2 className='text-[38px] font-semibold text-white mb-2.5'>Our Works</h2>
+                <p className='text-lg leading-[150%] text-[#E6E6E6] w-[780px]'>Discover a portfolio of visually stunning and strategically crafted digital projects that showcase our creativity and expertise.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
