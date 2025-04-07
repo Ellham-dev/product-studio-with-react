3d452269aadd6d7362d620b3e438c01e234ecc21
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-07 22:35:47 +0600

    SquareUp first part complete

--- src/components/SquareUpProcess.jsx
+++ src/components/SquareUpProcess.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const SquareUpProcess = () => {
   return (
-    <div>SquareUpProcess</div>
+    <>
+    <section className='bg-[#1E1E1E]'>
+        <div className="max-w-[1280px] mx-auto">
+          <div className="border-x border-[#333333] px-10 pt-20 pb-10">
+            <h2 className='text-[38px] font-semibold mb-2.5 text-white'>At SquareUp</h2>
+            <p className='text-base leading-[150%] text-[#E6E6E6] mb-10 w-[950px]'>We follow a structured and collaborative process to ensure the successful delivery of exceptional digital products. Our process combines industry best practices, creative thinking, and a client-centric approach.</p>
+            <span className='text-lg bg-[#262626] p-2.5 w-[341px] rounded-lg text-white'>Here's an overview of our typical process:</span>
+          </div>
+          <div className="">
+            
+          </div>
+          </div>
+          </section>
+
+    </>
   )
 }
 
