9b4792cb36df03d6acdd5c5e5c16982e489828bd
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-09 23:32:02 +0600

    SquareUp about section first part complete

--- src/components/AboutSquareUp.jsx
+++ src/components/AboutSquareUp.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import AboutSquareUPImg from '/AboutSquareUPImg.png'
 
 const AboutSquareUp = () => {
   return (
-    <div>AboutSquareUp</div>
+    <>
+      <section className='bg-[#1E1E1E]'>
+        <div className="max-w-[1280px] mx-auto">
+          <div className="flex items-center justify-between px-[150px] py-[100px]">
+            <div className="w-[598px]">
+              <h2 className='text-5xl font-semibold text-white mb-5'>About SquareUp</h2>
+              <p className='text-xl leading-[150%] text-[#E6E6E6]'>SquareUp is a digital product agency that is passionate about crafting exceptional digital experiences. We specialize in design, engineering, and project management, helping businesses thrive in the digital landscape. At SquareUp, we follow a structured and collaborative process to ensure the successful delivery of exceptional digital products. Our process combines industry best practices, creative thinking, and a client-centric approach.</p>
+            </div>
+            <div className="w-[598px]">
+              <img src={AboutSquareUPImg} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
