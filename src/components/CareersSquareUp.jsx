ba059050d46a15eb5465be3914112faf4f16fbd1
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-13 23:25:33 +0600

    CareersSquareUp Components complets

--- src/components/CareersSquareUp.jsx
+++ src/components/CareersSquareUp.jsx
@@ -5,15 +5,28 @@ const CareersSquareUp = () => {
     <>
       <section className='bg-[#1E1E1E]'>
         <div className="max-w-[1280px] mx-auto">
-          <div className="">
-            <div className="border border-[#333333] pt-20 pb-10 px-10">
-              <h3 className='text-[38px] font-semibold text-white mb-2.5'>Welcome to SquareUp, where talent meets opportunity!</h3>
-              <p className='text-base leading-[150%] text-[#E6E6E6] mb-10 w-[950px]'>At SquareUp, we believe that the success of our agency lies in the talent, passion, and dedication of our team members. We are a digital product agency that thrives on innovation, creativity, and collaboration. If you're ready to make a difference and contribute to cutting-edge projects, we invite you to explore career opportunities with us.</p>
-              <span className='text-lg bg-[#262626] p-2.5 w-[206px] rounded-lg text-white'>Why Work at SquareUp?</span>
-            </div>
+
+          <div className="border border-[#333333] pt-20 pb-10 px-10">
+            <h3 className='text-[38px] font-semibold text-white mb-2.5'>Welcome to SquareUp, where talent meets opportunity!</h3>
+            <p className='text-base leading-[150%] text-[#E6E6E6] mb-10 w-[950px]'>At SquareUp, we believe that the success of our agency lies in the talent, passion, and dedication of our team members. We are a digital product agency that thrives on innovation, creativity, and collaboration. If you're ready to make a difference and contribute to cutting-edge projects, we invite you to explore career opportunities with us.</p>
+            <span className='text-lg bg-[#262626] p-2.5 w-[206px] rounded-lg text-white'>Why Work at SquareUp?</span>
+          </div>
+          <div className="flex flex-wrap">
             <div className="border border-[#333333] px-10 py-20 w-[640px]">
               <h4 className='text-[32px] leading-[150%] font-medium text-[#D8FF99] w-[560px] border-b border-[#333333] pb-10 mb-[30px]'>Innovative and Impactful Projects</h4>
               <p className='text-base leading-[150%] text-[#98989A] w-[560px] '>At SquareUp, you'll have the opportunity to work on exciting and impactful projects that shape the digital landscape. From designing intuitive user interfaces to developing robust software solutions, you'll be part of a team that creates products that make a difference.</p>
+            </div>
+            <div className="border border-[#333333] px-10 py-20 w-[640px]">
+              <h4 className='text-[32px] leading-[150%] font-medium text-[#D8FF99] w-[560px] border-b border-[#333333] pb-10 mb-[30px]'>Supportive Environment</h4>
+              <p className='text-base leading-[150%] text-[#98989A] w-[560px] '>At SquareUp, you'll have the opportunity to work on exciting and impactful projects that shape the digital landscape. From designing intuitive user interfaces to developing robust software solutions, you'll be part of a team that creates products that make a difference.</p>
+            </div>
+            <div className="border border-[#333333] px-10 py-20 w-[640px]">
+              <h4 className='text-[32px] leading-[150%] font-medium text-[#D8FF99] w-[560px] border-b border-[#333333] pb-10 mb-[30px]'>Continuous Learning and Growth</h4>
+              <p className='text-base leading-[150%] text-[#98989A] w-[560px] '>We believe in investing in our team's growth and development. We provide opportunities for continuous learning, whether it's through workshops, training programs, or attending industry conferences. At SquareUp, you'll have the chance to expand your skill set and stay up-to-date with the latest trends and technologies.</p>
+            </div>
+            <div className="border border-[#333333] px-10 py-20 w-[640px]">
+              <h4 className='text-[32px] leading-[150%] font-medium text-[#D8FF99] w-[560px] border-b border-[#333333] pb-10 mb-[30px]'>Challenging and Rewarding Work</h4>
+              <p className='text-base leading-[150%] text-[#98989A] w-[560px] '>Our projects are challenging, but the rewards are even greater. We tackle complex problems and push ourselves to deliver innovative solutions. You'll be empowered to take ownership of your work, make a real impact, and see your ideas come to life.</p>
             </div>
           </div>
         </div>
