9b7afa69fee5e2af220b3c4b4eb9616d003f3a21
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-13 23:19:46 +0600

    Careers Hero update

--- src/components/CareersHero.jsx
+++ src/components/CareersHero.jsx
@@ -12,7 +12,7 @@ const CareersHero = () => {
             </div>
             <div className="absolute top-0 left-0 w-full h-full bg-black/80">
               <div className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                <h2 className='text-[48px] font-semibold text-white mb-2.5'>Join Our Team at SquareUp</h2>
+                <h2 className='text-[38px] font-semibold text-white mb-2.5'>Join Our Team at SquareUp</h2>
                 <p className='text-lg leading-6 text-[#E6E6E6]'>Unlock your potential and join our team of innovators and problem solvers.</p>
               </div>
             </div>
