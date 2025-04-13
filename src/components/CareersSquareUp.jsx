63138cc1bb509eeeb6b20e777c411ae512882060
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-13 23:20:21 +0600

    CareersSquareUp first part complete

--- src/components/CareersSquareUp.jsx
+++ src/components/CareersSquareUp.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const CareersSquareUp = () => {
   return (
-    <div>CareersSquareUp</div>
+    <>
+      <section className='bg-[#1E1E1E]'>
+        <div className="max-w-[1280px] mx-auto">
+          <div className="">
+            <div className="border border-[#333333] pt-20 pb-10 px-10">
+              <h3 className='text-[38px] font-semibold text-white mb-2.5'>Welcome to SquareUp, where talent meets opportunity!</h3>
+              <p className='text-base leading-[150%] text-[#E6E6E6] mb-10 w-[950px]'>At SquareUp, we believe that the success of our agency lies in the talent, passion, and dedication of our team members. We are a digital product agency that thrives on innovation, creativity, and collaboration. If you're ready to make a difference and contribute to cutting-edge projects, we invite you to explore career opportunities with us.</p>
+              <span className='text-lg bg-[#262626] p-2.5 w-[206px] rounded-lg text-white'>Why Work at SquareUp?</span>
+            </div>
+            <div className="border border-[#333333] px-10 py-20 w-[640px]">
+              <h4 className='text-[32px] leading-[150%] font-medium text-[#D8FF99] w-[560px] border-b border-[#333333] pb-10 mb-[30px]'>Innovative and Impactful Projects</h4>
+              <p className='text-base leading-[150%] text-[#98989A] w-[560px] '>At SquareUp, you'll have the opportunity to work on exciting and impactful projects that shape the digital landscape. From designing intuitive user interfaces to developing robust software solutions, you'll be part of a team that creates products that make a difference.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
