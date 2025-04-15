68c03e4502a5a52a454620ccc67a21697034c301
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-15 23:10:39 +0600

    CareersOpening components complete

--- src/components/CareersOpening.jsx
+++ src/components/CareersOpening.jsx
@@ -10,15 +10,104 @@ const CareersOpening = () => {
             <h3 className='text-[38px] font-semibold text-white mb-2.5'>Current Openings</h3>
             <p className='text-base leading-[150%] text-[#E6E6E6] mb-10 w-[990px]'>We are always on the lookout for talented individuals who are passionate about creating exceptional digital experiences. Whether you're a designer, engineer, project manager, or have skills that align with our agency's mission, we encourage you to explore our open positions.</p>
           </div>
-          <div className="border border-[#333333] p-10">
-            <h3 className='text-[22px] font-medium text-[#98989A]'>Design Job Openings</h3>
+          <div className="">
+            <div className="border border-[#333333] p-10">
+              <h3 className='text-[22px] font-medium text-[#98989A]'>Design Job Openings</h3>
+            </div>
+            <div className="flex items-center">
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+            </div>
           </div>
           <div className="">
-            <div className="w-[426.67px] border border-[#333333] p-10 ">
-              <img src={OpeningIcon} alt="" className='mb-6' />
-              <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
-              <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
-              <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+            <div className="border border-[#333333] p-10">
+              <h3 className='text-[22px] font-medium text-[#98989A]'>Development Job Openings</h3>
+            </div>
+            <div className="flex items-center">
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <div className="border border-[#333333] p-10">
+              <h3 className='text-[22px] font-medium text-[#98989A]'>Management Job Openings</h3>
+            </div>
+            <div className="flex items-center">
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <div className="border border-[#333333] p-10">
+              <h3 className='text-[22px] font-medium text-[#98989A]'>QA Job Openings</h3>
+            </div>
+            <div className="flex items-center">
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
+              <div className="w-[426.67px] border border-[#333333] p-10 ">
+                <img src={OpeningIcon} alt="" className='mb-6' />
+                <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+                <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+                <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+              </div>
             </div>
           </div>
         </div>
