a993a9df527e0a0f5d651576f875c7adcda25065
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-15 23:04:53 +0600

    One single Part complete

--- src/components/CareersOpening.jsx
+++ src/components/CareersOpening.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import OpeningIcon from '/OpeningIcon.png'
 
 const CareersOpening = () => {
   return (
@@ -11,6 +12,14 @@ const CareersOpening = () => {
           </div>
           <div className="border border-[#333333] p-10">
             <h3 className='text-[22px] font-medium text-[#98989A]'>Design Job Openings</h3>
+          </div>
+          <div className="">
+            <div className="w-[426.67px] border border-[#333333] p-10 ">
+              <img src={OpeningIcon} alt="" className='mb-6' />
+              <h2 className='text-xl leading-[150%] font-medium text-white mb-3.5'>UI Designer</h2>
+              <p className='text-base leading-[150%] text-[#E6E6E6] w-[346.67px mb-[54px]'>Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
+              <button className='text-lg leading-6 text-white bg-[#262626] py-3.5 w-full rounded-lg'>Apply Now</button>
+            </div>
           </div>
         </div>
       </section>
