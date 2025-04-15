795cf1b8254c9cea427686b6f6c11ac6e5255d52
Ellham Khan <ellhamkhan444@gmail.com>
2025-04-15 22:53:07 +0600

    Careers Opening components first part complete

--- src/components/CareersOpening.jsx
+++ src/components/CareersOpening.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const CareersOpening = () => {
   return (
-    <div>CareersOpening</div>
+    <>
+      <section className='bg-[#1E1E1E]'>
+        <div className="max-w-[1280px] mx-auto">
+          <div className="border border-[#333333] pt-20 pb-10 px-10">
+            <h3 className='text-[38px] font-semibold text-white mb-2.5'>Current Openings</h3>
+            <p className='text-base leading-[150%] text-[#E6E6E6] mb-10 w-[990px]'>We are always on the lookout for talented individuals who are passionate about creating exceptional digital experiences. Whether you're a designer, engineer, project manager, or have skills that align with our agency's mission, we encourage you to explore our open positions.</p>
+          </div>
+          <div className="border border-[#333333] p-10">
+            <h3 className='text-[22px] font-medium text-[#98989A]'>Design Job Openings</h3>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
